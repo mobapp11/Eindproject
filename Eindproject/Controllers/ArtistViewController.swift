f93666d374bbe5375b8a316dee4297daee5c6558
mobapp11 <mobapp11@student.ehb.be>
2020-01-30 12:36:27 +0100

    j

--- Eindproject/Controllers/ArtistViewController.swift
+++ Eindproject/Controllers/ArtistViewController.swift
@@ -11,57 +11,59 @@ import UIKit
 class ArtistViewController: UIViewController, UITableViewDataSource{
     
     
-  
+    
     @IBOutlet weak var tableView: UITableView!
     let rijen:DatasourceArtiesten = DatasourceArtiesten.init()
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
     
     
-    func numberOfSections(in tableView: UITableView) ->Int{
-        return 1;
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rijen.artists.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currentCell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
-        let currentArtist = rijen.artists[indexPath.row]
-        
-        currentCell.nameLbl.text = currentArtist.name
-        currentCell.podiumLbl.text = currentArtist.podium
-        currentCell.hourLbl.text = currentArtist.hour
-        
-        return currentCell
-    }
-    
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "detailSegue"{
-                let selectedIndexPath:IndexPath = tableView.indexPath(for: sender as! TableViewCell)!
-                let selectedArtist = rijen.artists[selectedIndexPath.row]
-                let destinationVC:DetailViewController = segue.destination as! DetailViewController
-                destinationVC.selectedArtist = selectedArtist
-            }
-        }
-        
-    }
-        
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+func numberOfSections(in tableView: UITableView) ->Int{
+    return 1;
+}
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return rijen.artists.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let currentCell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+    let currentArtist = rijen.artists[indexPath.row]
+    
+    currentCell.nameLbl.text = currentArtist.name
+    currentCell.podiumLbl.text = currentArtist.podium
+    currentCell.hourLbl.text = currentArtist.hour
+    
+    return currentCell
+}
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "detailSegue"{
+        let selectedIndexPath:IndexPath = tableView.indexPath(for: sender as! TableViewCell)!
+        let selectedArtist = rijen.artists[selectedIndexPath.row]
+        let destinationVC:DetailViewController = segue.destination as! DetailViewController
+        destinationVC.selectedArtist = selectedArtist
     }
-    */
+}
+}
+
+
+
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 
