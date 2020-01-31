ed35d51722ad84fc5db7d89d345ebc7d4d0e8374
mobapp11 <mobapp11@student.ehb.be>
2020-01-31 11:30:08 +0100

    datasource and constraints fixed

--- Eindproject/Model/DatasourceFeed.swift
+++ Eindproject/Model/DatasourceFeed.swift
@@ -17,15 +17,15 @@ class DatasourceFeed {
         
         let post1 = Feed.init(image: "vis", title: "New artist confirmed!")
             posts.append(post1)
-        let post2 = Feed.init(image: "vis.jpg", title: "New artist confirmed!")
+        let post2 = Feed.init(image: "jason-blackeye-IShAuJTJIE4-unsplash.jpg", title: "Only a few tickets left!!")
             posts.append(post2)
-        let post3 = Feed.init(image: "vis.jpg", title: "New artist confirmed!")
+        let post3 = Feed.init(image: "pawel-janiak-xl4rP5WCekY-unsplash.jpg", title: "New stage this year named salt stage!")
             posts.append(post3)
-        let post4 = Feed.init(image: "vis.jpg", title: "New artist confirmed!")
+        let post4 = Feed.init(image: "kai", title: "Last VIP-tickets available!!")
             posts.append(post4)
-        let post5 = Feed.init(image: "vis.jpg", title: "New artist confirmed!")
+        let post5 = Feed.init(image: "anthony-delanoix-hzgs56Ze49s-unsplash.jpg", title: "Sweminem also confirmed!!")
             posts.append(post5)
-        let post6 = Feed.init(image: "vis.jpg", title: "New artist confirmed!")
+        let post6 = Feed.init(image: "ron-smith-tknOyEefp2k-unsplash.jpg", title: "Only 3 months left until Vistival!")
             posts.append(post6)
     }
 }
